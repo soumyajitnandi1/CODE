46ee52e74986e65375d2cdbb01d55a08fc50d52c
Infinity_Tech <71644634+infinity-sudo@users.noreply.github.com>
2021-10-04 12:25:16 +0530

    Exponent calculation in faster way

---
+++ faster_exponent_calculation.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x, n;
+    cout<<"Enter the no = ";
+    cin>>x;
+    cout<<"Enter the value of exponent = ";
+    cin>>n;
+    int ans=1;
+    while(n>0){
+        int bit=n&1;
+        if(bit){
+            ans=ans*x;
+        }
+        n=n>>1;
+        x=x*x;
+    }
+    cout<<ans;
+    return 0;
+}
